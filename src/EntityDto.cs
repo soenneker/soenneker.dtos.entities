fbe172aa8b4100be9f09bb9794ba7828af3a73cd
Jake Soenneker <jake@soenneker.com>
2025-08-16 12:52:40 -0500

    Initial commit

---
+++ src/EntityDto.cs
@@ -0,0 +1,21 @@
+﻿using Soenneker.Attributes.PublicOpenApiObject;
+using System;
+using System.Text.Json.Serialization;
+
+namespace Soenneker.Dtos.Entities;
+
+/// <summary>
+/// A hard type mapping to Entity
+/// </summary>
+[PublicOpenApiObject]
+public class EntityDto
+{
+    [JsonPropertyName("id")]
+    public virtual string Id { get; set; } = null!;
+
+    [JsonPropertyName("createdAt")]
+    public virtual DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("modifiedAt")]
+    public virtual DateTime? ModifiedAt { get; set; }
+}
---
+++ test/Soenneker.Dtos.Entities.Tests/EntityDtoTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Dtos.Entities.Tests;
+
+public sealed class EntityDtoTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
